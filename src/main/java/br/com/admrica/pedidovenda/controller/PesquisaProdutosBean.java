63e5f559640d3255db836160c1f5e7776345eb74
Gabriel Veras <gabrielverasm@gmail.com>
2017-07-03 18:14:23 -0300

    Pesquisando produto e corrigindo erro de mensagem. 12.8 ok

--- src/main/java/br/com/admrica/pedidovenda/controller/PesquisaProdutosBean.java
+++ src/main/java/br/com/admrica/pedidovenda/controller/PesquisaProdutosBean.java
@@ -1,29 +1,42 @@
 package br.com.admrica.pedidovenda.controller;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import br.com.admrica.pedidovenda.model.Produto;
+import br.com.admrica.pedidovenda.repository.Produtos;
+import br.com.admrica.pedidovenda.repository.filter.ProdutoFilter;
 
 @Named
 @ViewScoped
 public class PesquisaProdutosBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	private List<Integer> produtosFiltrados;
-	
+
+	@Inject
+	private Produtos produtos;
+
+	private ProdutoFilter filtro;
+	private List<Produto> produtosFiltrados;
+
 	public PesquisaProdutosBean() {
-		produtosFiltrados = new ArrayList<>();
-		for (int i = 0; i < 50; i++) {
-			produtosFiltrados.add(i);
-		}
+		filtro = new ProdutoFilter();
 	}
 
-	public List<Integer> getProdutosFiltrados() {
+	public void pesquisar() {
+		produtosFiltrados = produtos.filtrados(filtro);
+	}
+
+	public List<Produto> getProdutosFiltrados() {
 		return produtosFiltrados;
 	}
-	
+
+	public ProdutoFilter getFiltro() {
+		return filtro;
+	}
+
 }
