afd7fa16723741909a884dbf32905183843245b8
Gabriel Veras <gabrielverasm@gmail.com>
2017-07-06 01:20:32 -0300

    Corrigindo e nromalizano

---
+++ src/main/java/br/com/admrica/pedidovenda/controller/CadastroProdutoBean.java
@@ -0,0 +1,103 @@
+package br.com.admrica.pedidovenda.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.validation.constraints.NotNull;
+
+import br.com.admrica.pedidovenda.model.Categoria;
+import br.com.admrica.pedidovenda.model.Produto;
+import br.com.admrica.pedidovenda.repository.Categorias;
+import br.com.admrica.pedidovenda.service.CadastroProdutoService;
+import br.com.admrica.pedidovenda.util.jsf.FacesUtil;
+
+@Named
+@ViewScoped
+public class CadastroProdutoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private Categorias categorias;
+
+	private Produto produto;
+
+	@NotNull
+	private Categoria categoriaPai;
+
+	@Inject
+	private CadastroProdutoService cadastroProdutoService;
+
+	private List<Categoria> categoriasRaizes;
+	private List<Categoria> subcategorias;
+
+	public CadastroProdutoBean() {
+		limpar();
+	}
+
+	public void inicializar() {
+		System.out.println("Inicializando...");
+		if (FacesUtil.isNotPostback()) {
+			categoriasRaizes = categorias.raizes();
+
+			if (this.categoriaPai != null) {
+				carregarSubcategorias();
+			}
+		}
+	}
+
+	public void limpar() {
+		produto = new Produto();
+		categoriaPai = null;
+		subcategorias = new ArrayList<>();
+
+	}
+
+	public void salvar() {
+		this.produto = cadastroProdutoService.salvar(this.produto);
+		limpar();
+
+		FacesUtil.addInfoMessage("Produto salvo com sucesso!");
+	}
+
+	public void carregarSubcategorias() {
+		subcategorias = categorias.subcategoriasDe(categoriaPai);
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+
+		if (this.produto != null) {
+			this.categoriaPai = this.produto.getCategoria().getCategoriaPai();
+		}
+
+	}
+
+	public List<Categoria> getCategoriasRaizes() {
+		return categoriasRaizes;
+	}
+
+	public Categoria getCategoriaPai() {
+		return categoriaPai;
+	}
+
+	public void setCategoriaPai(Categoria categoriaPai) {
+		this.categoriaPai = categoriaPai;
+	}
+
+	public List<Categoria> getSubcategorias() {
+		return subcategorias;
+	}
+
+	public boolean isEditando() {
+		return this.produto.getId() != null;
+	}
+}
